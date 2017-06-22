2f928f2a5466f102d893684bd96f3606c20a35cc
刘佳 <liujia@jiedaibao.com>
2017-06-22 15:47:13 +0800

    opera提交

--- opera.js
+++ opera.js
@@ -8,7 +8,7 @@ messageQueue.process(function (job, done) {
   console.log(job.data);
   var client = new net.Socket();
   client.connect(constValue.opera.port, constValue.opera.host, function() {
-    let xml = "<Message type='PostSimple' direction='ToPMS'><PostSimple RoomNum='1111111' PostType='C' SalesOutlet='03' PostNum='Z0256984' CheckNum='Z0256984' PaymentMethod='7080' ServingTime='3' TableNum='112' Covers='' TotalAmount='206600' Subtotal2='196000' Date='170605' Time='162556' UserID='tjw' ClearText='tjw' /></Message>";
+    let xml = "<Message type='PostSimple' direction='ToPMS'><PostSimple RoomNum='999994' PostType='C' SalesOutlet='03' PostNum='Z0256984' CheckNum='Z0256984' PaymentMethod='7080' ServingTime='3' TableNum='112' Covers='' TotalAmount='206600' Subtotal2='196000' Date='170605' Time='162556' UserID='tjw' ClearText='tjw' /></Message>";
     client.write(Buffer.concat(Buffer.from(xml),Buffer.from([0x00])));
   });
 
