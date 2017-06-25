edd1e524ddcaaa0d5bba180884ecac194e867e06
刘佳 <liujia@jiedaibao.com>
2017-06-25 22:58:40 +0800

    连接java服务器代码

--- opera.js
+++ opera.js
@@ -3,13 +3,27 @@ var Queue = require('bull');
 var messageQueue = new Queue('opera', {redis: constValue.redis});
 var net = require('net');
 
-messageQueue.process(function (job, done) {
+messageQueue.process(function (j, done) {
   console.log('consume job');
-  console.log(job.data);
-  if (job.data.type === 'tcp') {
+  console.log(j.data);
+  let job = j.data;
+  if (job.type === 'tcp') {
+    job = job.job;
     var client = new net.Socket();
     client.connect(constValue.opera.port, constValue.opera.host, function() {
-      let xml = "<Message type='PostSimple' direction='ToPMS'><PostSimple RoomNum='9533' PostType='C' SalesOutlet='03' PostNum='Q0016561' CheckNum='Q0016561' PaymentMethod='7080' ServingTime='2' TableNum='' Covers='' TotalAmount='246000' Subtotal1='246000'  Date='170622' Time='161735' UserID='chai' ClearText='chai' /></Message>";
+
+      let Subtotal = '';
+      Object.keys(job.products.Subtotal).forEach((SubtotalIndex) => {
+        try {
+          let amount = job.products.Subtotal[SubtotalIndex];
+          SubtotalIndex = parseInt(SubtotalIndex);
+          Subtotal += "Subtotal"+SubtotalIndex+"='"+amount+"' ";
+        } catch (e) {
+
+        }
+      });
+      let xml = "<Message type='PostSimple' direction='ToPMS'><PostSimple RoomNum='"+job.RoomNum+"' PostType='C' SalesOutlet='"+job.products.SalesOutlet+"' PostNum='"+job.PostNum+"' CheckNum='"+job.CheckNum+"' PaymentMethod='"+job.PaymentMethod+"' ServingTime='"+job.products.ServingTime+"' TableNum='' Covers='' TotalAmount='"+job.products.TotalAmount+"' "+Subtotal+"  Date='"+job.Date+"' Time='"+job.Time+"' UserID='"+job.UserID+"' ClearText='"+job.UserID+"' /></Message>";
+      //let xml = "<Message type='PostSimple' direction='ToPMS'><PostSimple RoomNum='9533' PostType='C' SalesOutlet='03' PostNum='Q0016562' CheckNum='Q0016562' PaymentMethod='7080' ServingTime='3' TableNum='' Covers='' TotalAmount='164800' Subtotal3='1800' Subtotal2='98000' Subtotal1='65000'  Date='170622' Time='165029' UserID='chai' ClearText='chai' /> </Message>";
       client.write(Buffer.concat([Buffer.from(xml),Buffer.from([0x00])]));
     });
 
@@ -19,7 +33,7 @@ messageQueue.process(function (job, done) {
     });
 
     client.on('error', function(err) {
-      job.data.retry = job.data.retry?job.retry+1:1;
+      job.products.ServingTime += 1;
       console.log('err: ' + err.message);
       //messageQueue.add(job);
     });
@@ -34,8 +48,29 @@ messageQueue.process(function (job, done) {
 
 module.exports = {
   add: function (job) {
+
+    let jobs = {};
+    job.products.forEach((product) => {
+      let category = product.Subtotal2;
+      if (category && category.length == 4) {
+        let SalesOutlet = category.substr(0,2);
+        let Subtotal = category.substr(2,2);
+        if (!jobs[SalesOutlet]) {
+          jobs[SalesOutlet] = {SalesOutlet, TotalAmount: 0, ServingTime: 1, Subtotal:{}};
+        }
+        jobs[SalesOutlet].TotalAmount += product.TotalAmount;
+        if (!jobs[SalesOutlet].Subtotal[Subtotal]) {
+          jobs[SalesOutlet].Subtotal[Subtotal] = 0;
+        }
+        jobs[SalesOutlet].Subtotal[Subtotal] += product.TotalAmount;
+      }
+    });
     console.log('add job');
-    console.log(job);
-    messageQueue.add({type: 'tcp', job: job});
+    Object.keys(jobs).forEach((key) => {
+      console.log(jobs[key]);
+      let jobItem = Object.assign({}, job);
+      jobItem.products = jobs[key];
+      messageQueue.add({type: 'tcp', job: jobItem});
+    });
   }
 };
--- soapService.js
+++ soapService.js
@@ -2,6 +2,16 @@ var utils = require('./utils');
 var constValue = require('./const');
 var moment = require('moment-timezone');
 
+var trimDot = function (str) {
+  while (str.startsWith(',')) {
+    str = str.substr(1, str.length-1);
+  }
+  while (str.endsWith(',')) {
+    str = str.substr(0, str.length-1);
+  }
+  return str;
+};
+
 var Test = function(args, callback) {
   callback({
     TestResult: 1
@@ -9,19 +19,112 @@ var Test = function(args, callback) {
 };
 
 var GetAmountStatistical = function (args, callback) {
-  callback({
-    GetAmountStatisticalResult: 1
+  console.log('GetAmountStatistical coming');
+  console.log(JSON.stringify(args));
+  utils.getRequest(function (err, request) {
+
+    if (err) {
+      console.log(err);
+      callback({
+        GetAmountStatisticalResult: 1
+      });
+      return;
+    }
+
+    request({
+      method: 'POST',
+      uri: constValue.tkBaseUrl + 'Api/Front/Checkout/PracticeCategorySalesData',
+      json: true,
+      body: {
+        leagueId: constValue.tkLeagueId,
+        clubId: constValue.tkClubId,
+        person: 0,
+        checkout: 0,
+        report_id: 400,
+        start_date: moment(args.starDate).format('YYYY-MM-DD HH:mm:ss'),
+        end_date: moment(args.endDate).format('YYYY-MM-DD HH:mm:ss'),
+      }
+    }, function (error, response, body) {
+      if (!error && body && body.code) {
+        let infos = [];
+        body.message.outlets.forEach(function (item) {
+          infos.push({AmountList:{
+            Code: 0,
+            Name: item.outlet_name,
+            Amount: item.money?item.money:0,
+          }})
+        });
+        callback({
+          GetAmountPaymentResult: 0,
+          infos
+        });
+      } else {
+        console.log(err);
+        console.log(body);
+        callback({
+          GetAmountPaymentResult: 1
+        });
+      }
+    });
   });
 };
 
 var GetPaymentSingle = function (args, callback) {
-  callback({
-    GetPaymentSingleResult: 1
+  console.log('GetPaymentSingle coming');
+  console.log(JSON.stringify(args));
+  utils.getRequest(function (err, request) {
+
+    if (err) {
+      console.log(err);
+      callback({
+        GetPaymentSingleResult: 1
+      });
+      return;
+    }
+
+    request({
+      method: 'POST',
+      uri: constValue.tkBaseUrl + '/Api/Front/Checkout/CountBussinessData',
+      json: true,
+      body: {
+        leagueId: constValue.tkLeagueId,
+        clubId: constValue.tkClubId,
+        person: 0,
+        checkout: 0,
+        paymentName: "会员卡",
+        start_date: moment(args.starDate).format('YYYY-MM-DD HH:mm:ss'),
+        end_date: moment(args.endDate).format('YYYY-MM-DD HH:mm:ss'),
+      }
+    }, function (error, response, body) {
+      if (!error && body && body.code) {
+        let infos = [];
+        body.message.forEach(function (item) {
+          infos.push({AmountList:{
+            Code: trimDot(item.memberCard),
+            Name: trimDot(item.memberName),
+            Amount: item.money?item.money*100:0,
+          }})
+        });
+        callback({
+          GetPaymentSingleResult: 0,
+          infos
+        });
+      } else {
+        console.log(err);
+        console.log(body);
+        callback({
+          GetPaymentSingleResult: 1
+        });
+      }
+    });
   });
+
+
 };
 
 var GetAmountPayment = function(args, callback) {
   console.log('GetAmountPayment coming');
+  console.log(JSON.stringify(args));
   utils.getRequest(function (err, request) {
 
     if (err) {
@@ -39,8 +142,8 @@ var GetAmountPayment = function(args, callback) {
       body: {
         leagueId: constValue.tkLeagueId,
         clubId: constValue.tkClubId,
-        person: '0',
-        checkout: '0',
+        person: 0,
+        checkout: 0,
         start_date: moment(args.starDate).format('YYYY-MM-DD HH:mm:ss'),
         end_date: moment(args.endDate).format('YYYY-MM-DD HH:mm:ss'),
       }
@@ -51,7 +154,7 @@ var GetAmountPayment = function(args, callback) {
           infos.push({AmountList:{
             Code: 0,
             Name: item.name,
-            Amount: item.money?item.money:0,
+            Amount: item.money?item.money*100:0,
           }})
         });
         callback({
