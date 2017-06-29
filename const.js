d147ae893c7d832fb4d7efa4318f8713c55b87ce
刘佳 <liujia@jiedaibao.com>
2017-06-29 10:11:35 +0800

    修改时间格式

--- const.js
+++ const.js
@@ -1,12 +1,13 @@
 
 var constObject = {
-    //tkBaseUrl : 'http://106.75.55.62:8080',
-    tkBaseUrl : 'http://127.0.0.1:8080',
-    //tkLeagueId : 87,
-    tkLeagueId : 999,
+    tkBaseUrl : 'http://106.75.55.62:8080',
+    //tkBaseUrl : 'http://127.0.0.1:8080',
+    tkLeagueId : 87,
+    //tkLeagueId : 999,
     tkClubId : 1,
     tkUser : 'admin',
-    tkPassword : '654123',
+    tkPassword : '123456',
+    //tkPassword : '654123',
     redis: {
         port: 6379,
         host: '127.0.0.1',
